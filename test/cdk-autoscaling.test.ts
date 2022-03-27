9ff5648d256c4ff41739da453dd273880d964641
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-03-27 17:21:57 +0900

    Initial commit

---
+++ test/cdk-autoscaling.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAutoscaling from '../lib/cdk-autoscaling-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-autoscaling-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAutoscaling.CdkAutoscalingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
