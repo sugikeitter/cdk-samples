7b4911a49c784b62dd0def3edfb0c64d6448ab86
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-04-17 14:38:49 +0900

    Update ECS stack

--- lib/ecs-autoscaling-stack.ts
+++ lib/ecs-autoscaling-stack.ts
@@ -3,6 +3,7 @@ import {
   Stack,
   StackProps,
   aws_ec2 as ec2,
+  aws_ecr as ecr,
   aws_ecs as ecs,
   aws_ecs_patterns as ecs_patterns
 
@@ -13,22 +14,35 @@ export class EcsAsgStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpcName = process.env.MY_VPC_NAME;
+    const MY_VPC_NAME = process.env.MY_VPC_NAME;
+    const MY_ECR_REPOSITORY_NAME = process.env.MY_ECR_REPOSITORY_NAME || ""
 
     // TODO vpc名をCFnテンプレート作成時に渡す
     const vpc: ec2.IVpc = ec2.Vpc.fromLookup(this, 'Vpc', {
-      vpcName: vpcName
+      vpcName: MY_VPC_NAME
     });
 
+    // CI/CDを見越して、CFnの状態をリソースと同期させたいなら、ECSのクラスタとALBとListner/TargetGroupのみの定義（変更があまりない）
+    // ECSサービスとCodePipelineを別Stackで用意するのが良さそう（変更が多い？）
     new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'EcsAlbFargateSvcPattern', {
       vpc: vpc,
       taskSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
       },
       taskImageOptions: {
-        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        image: ecs.ContainerImage.fromEcrRepository(
+          ecr.Repository.fromRepositoryName(this, "EcrRepo", MY_ECR_REPOSITORY_NAME),
+          "latest", // tag
+        ),
       },
-    })
+      desiredCount: 3,
+      deploymentController: {
+        type: ecs.DeploymentControllerType.CODE_DEPLOY,
+      },
+      // loadBalancer: // Blue/Greenデプロイのためにポート2つ用意したALBが必要な場合は、別途用意が必要
+    });
+
+    // TODO ECSでBlue/GreenするためのCodeDeployを追加したい（でも最終的にはCodePipeline？）
 
   }
 }
