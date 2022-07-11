8c5e80d782f999d7403580d4f8220d3c6f6f1cf8
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-07-11 23:22:09 +0900

    Create iam-demo-stack.ts

---
+++ lib/iam-demo-stack.ts
@@ -0,0 +1,37 @@
+import {
+  Stack,
+  StackProps,
+  aws_iam as iam,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class IamDemoStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    // IAMユーザーをdemo-***という名前で権限をEC2のReadOnlyで手動で作るけど、EC2のReadOnly+起動/停止できるロール
+    const role = new iam.Role(this, 'DemoRoleForEC2RunStartStop', {
+      assumedBy: new iam.ArnPrincipal("arn:aws:iam::" + props.env?.account + ":user/demo-ec2-readonly"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess"),
+      ],
+      inlinePolicies: {
+        "ec2RunStartStop": iam.PolicyDocument.fromJson({
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": [
+                "ec2:RunInstances",
+                "ec2:StartInstances",
+                "ec2:StopInstances"
+              ],
+              "Resource": "*"
+            }
+          ]
+        })
+      }
+    });
+    // TODO パーミッション境界
+  }
+}
