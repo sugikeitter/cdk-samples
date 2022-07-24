9c1e5c75c210948144a10b5eaec61df8c24005d7
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-07-25 03:14:25 +0900

    Update iam-demo-stack.ts

--- lib/iam-demo-stack.ts
+++ lib/iam-demo-stack.ts
@@ -16,6 +16,7 @@ export class IamDemoStack extends Stack {
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess"),
       ],
+      // EC2インスタンス起動のためにRoleを作成できてしまうポリシーが割り当てられている
       inlinePolicies: {
         "ec2RunStartStop": iam.PolicyDocument.fromJson({
           "Version": "2012-10-17",
@@ -29,9 +30,26 @@ export class IamDemoStack extends Stack {
                 "ec2:StopInstances",
                 "iam:ListInstanceProfiles",
                 "iam:GetRole",
-                "iam:PassRole"
+                // "iam:PassRole",
+                // "iam:CreateRole",
+                // "iam:CreateInstanceProfile",
+                "iam:ListPolicies",
+                "iam:ListRoles",
+                "iam:AttachRolePolicy"
               ],
               "Resource": "*"
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "iam:PassRole"
+              ],
+              "Resource": [
+                "arn:aws:iam::" + props.env?.account + ":role/AmazonSSMRoleForInstancesQuickSetup"
+              ],
+              "Condition": {
+                "StringEquals": {"iam:PassedToService": "ec2.amazonaws.com"},
+              }
             }
           ]
         })
@@ -51,6 +69,13 @@ export class IamDemoStack extends Stack {
               "ec2:StartInstances",
               "ec2:StopInstances",
               "iam:ListInstanceProfiles",
+              "iam:GetRole",
+              // "iam:PassRole", // EC2, LambdaのみOKのConditionを下で設定
+              "iam:CreateRole",
+              "iam:CreateInstanceProfile",
+              "iam:ListPolicies",
+              "iam:ListRoles",
+              "iam:AttachRolePolicy"
             ],
             "Resource": "*"
           },
@@ -59,7 +84,10 @@ export class IamDemoStack extends Stack {
             "Action": [
               "iam:PassRole"
             ],
-            "Resource": "*",
+            "Resource": [
+              "arn:aws:iam::" + props.env?.account + ":role/demoBoundaryRole*",
+              "arn:aws:iam::" + props.env?.account + ":role/AmazonSSMRoleForInstancesQuickSetup"
+            ],
             "Condition": {
               "StringEquals": {"iam:PassedToService": "ec2.amazonaws.com"},
             }
@@ -69,21 +97,106 @@ export class IamDemoStack extends Stack {
             "Action": [
               "iam:PassRole"
             ],
-            "Resource": "*",
+            "Resource": "arn:aws:iam::" + props.env?.account + ":role/demoBoundaryRole*",
             "Condition": {
               "StringEquals": {"iam:PassedToService": "lambda.amazonaws.com"},
             }
+          },
+          {
+            "Sid": "DenyPermBoundaryIAMPolicyAlteration",
+            "Effect": "Deny",
+            "Action": [
+              "iam:DeletePolicy",
+              "iam:DeletePolicyVersion",
+              "iam:CreatePolicyVersion",
+              "iam:SetDefaultPolicyVersion"
+            ],
+            "Resource": [
+              "arn:aws:iam::" + props.env?.account + ":policy/DemoPlicyForBoundary"
+            ]
+          },
+          {
+            "Sid": "DenyRemovalOfPermBoundaryFromAnyUserOrRole",
+            "Effect": "Deny",
+            "Action": [
+              "iam:DeleteUserPermissionsBoundary",
+              "iam:DeleteRolePermissionsBoundary"
+            ],
+            "Resource": [
+              "arn:aws:iam::" + props.env?.account + ":user/*",
+              "arn:aws:iam::" + props.env?.account + ":role/*"
+            ],
+            "Condition": {
+              "StringEquals": {
+                "iam:PermissionsBoundary": "arn:aws:iam::" + props.env?.account + ":policy/DemoPlicyForBoundary"
+              }
+            }
+          },
+          {
+            "Sid": "DenyAccessIfRequiredPermBoundaryIsNotBeingApplied",
+            "Effect": "Deny",
+            "Action": [
+              "iam:PutUserPermissionsBoundary",
+              "iam:PutRolePermissionsBoundary",
+              "iam:CreateUser"
+            ],
+            "Resource": [
+              "arn:aws:iam::" + props.env?.account + ":user/*",
+              "arn:aws:iam::" + props.env?.account + ":role/*"
+            ],
+            "Condition": {
+              "StringNotEquals": {
+                "iam:PermissionsBoundary": "arn:aws:iam::" + props.env?.account + ":policy/DemoPlicyForBoundary"
+              }
+            }
+          },
+          {
+            "Sid": "DenyRoleCreationWithOutPrefix",
+            "Effect": "Deny",
+            "Action": [
+              "iam:CreateRole"
+            ],
+            "NotResource": [
+              "arn:aws:iam::" + props.env?.account + ":role/demoBoundaryRole*"
+            ]
+          },
+          {
+            "Sid": "DenyRoleCreationWithOutPermBoundary",
+            "Effect": "Deny",
+            "Action": [
+              "iam:CreateRole"
+            ],
+            "Resource": [
+              "*"
+            ],
+            "Condition": {
+              "StringNotEquals": {
+                "iam:PermissionsBoundary": "arn:aws:iam::" + props.env?.account + ":policy/DemoPlicyForBoundary"
+              }
+            }
+          },
+          {
+            "Effect": "Allow",
+            "Action": [
+              "ec2:RunInstances",
+              "ec2:CreateTags",
+              "ec2:StartInstances",
+              "ec2:StopInstances",
+              "iam:ListInstanceProfiles",
+            ],
+            "Resource": "*"
           }
         ]
       })
     });
 
-    const roleWithPermissionBoundary = new iam.Role(this, 'DemoRoleForEC2RunStartStop', {
-      roleName: "DemoRoleForEC2RunStartStop",
+    const roleWithPermissionBoundary = new iam.Role(this, 'DemoRoleForEC2RunStartStopWithPermBooundary', {
+      roleName: "DemoRoleForEC2RunStartStopWithPermBooundary",
       assumedBy: new iam.ArnPrincipal("arn:aws:iam::" + props.env?.account + ":user/demo-ec2-readonly"),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess"),
       ],
+      // EC2インスタンス起動のためにRoleを作成できてしまうポリシーが割り当てられているが、PermissionBoundaryが付いている
       inlinePolicies: {
         "ec2RunStartStop": iam.PolicyDocument.fromJson({
           "Version": "2012-10-17",
@@ -96,14 +209,19 @@ export class IamDemoStack extends Stack {
                 "ec2:StartInstances",
                 "ec2:StopInstances",
                 "iam:ListInstanceProfiles",
-                "iam:PassRole"
+                "iam:GetRole",
+                "iam:PassRole",
+                "iam:CreateRole",
+                "iam:CreateInstanceProfile",
+                "iam:ListPolicies",
+                "iam:ListRoles",
+                "iam:AttachRolePolicy"
               ],
               "Resource": "*"
             }
           ]
         })
       },
-      // TODO パーミッション境界
       permissionsBoundary: policyForBoundary
     });
   }
